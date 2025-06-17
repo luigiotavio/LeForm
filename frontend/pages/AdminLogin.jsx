9d0dd2e08941ff6b01aa2ce6a05746162c5ed467
luigiotavio <luigiotavio4577@gmail.com>
2025-06-17 11:56:36 -0300

    fix: integração login

--- frontend/pages/AdminLogin.jsx
+++ frontend/pages/AdminLogin.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const LOGO_URL = '/leform_logo.svg';
 
 const AdminLogin = () => {
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPass, setShowPass] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setErrorMsg('');
     if (!username || !password) {
       setErrorMsg('Preencha usuário e senha.');
       return;
     }
-    setErrorMsg('');
-    alert('Login simulado! (adicione integração depois)');
+
+    try {
+      const response = await fetch('http://localhost:5000/authentication', {
+        method: 'POST',
+        headers: {
+          'Content-type':'application/json'
+        },
+        body: JSON.stringify({username, password}),     
+      });
+      const isAuthenticated = await response.json();
+      if (isAuthenticated === true){
+        navigate('/admin')
+      } else {
+        setErrorMsg('Usuário ou senha inválidos!.')
+      }
+
+    } catch(error){
+        console.error('Erro na autenticação:', error);
+        setErrorMsg('Erro no servidor. Tente novamente mais tarde.');
+    }
   };
 
   return (
