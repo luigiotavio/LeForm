918cdcba261f3ba6eb7bf22089265d34a909cbb4
luigiotavio <luigiotavio4577@gmail.com>
2025-08-05 10:51:26 -0300

    alterações backend CORS - 11

--- backend/routes/clinicasRoutes.py
+++ backend/routes/clinicasRoutes.py
@@ -5,6 +5,6 @@ clinica_bp = Blueprint('clinica', __name__, url_prefix='/clinicas')
 
 clinica_bp.get('/')(ClinicaController.get_all)
 clinica_bp.get('/<int:id>')(ClinicaController.get_by_id)
-clinica_bp.post('/')(ClinicaController.create)
+clinica_bp.post('')(ClinicaController.create)
 clinica_bp.put('/<int:id>')(ClinicaController.update)
 clinica_bp.delete('/<int:id>')(ClinicaController.delete)
--- backend/routes/cursoRoutes.py
+++ backend/routes/cursoRoutes.py
@@ -5,6 +5,6 @@ curso_bp = Blueprint('curso', __name__, url_prefix='/cursoslista')
 
 curso_bp.get('/')(CursoController.get_all)
 curso_bp.get('/<int:id>')(CursoController.get_by_id)
-curso_bp.post('/')(CursoController.create)
+curso_bp.post('')(CursoController.create)
 curso_bp.put('/<int:id>')(CursoController.update)
 curso_bp.delete('/<int:id>')(CursoController.delete)
